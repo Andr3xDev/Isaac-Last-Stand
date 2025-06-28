0827cae09979fa3432ce743876130b026c2ba9f8
Andr3xC <achavarropp@gmail.com>
2025-06-28 05:31:38 -0500

    add: basic menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
-export const Home = () => {
+import Button from "../components/Buton";
+import Menu from "../components/Menu";
+
+function Home() {
     return (
-        <div className="bg-black \">
-            <p>Este contenido es protegido.</p>
+        <div className="w-screen h-screen flex justify-center items-start">
+            <div className="w-1/2 flex flex-col items-center">
+                <img
+                    src="/src/assets/title.png"
+                    alt="Logo game"
+                    className="w-10/12 py-6"
+                />
+
+                <Menu>
+                    <Button label="Crear Sala" to="/lobby" />
+                    <Button label="Unirse a una sala" to="/lobby" />
+                </Menu>
+            </div>
         </div>
     );
-};
+}
+
+export default Home;
