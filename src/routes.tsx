f82c9459e9b10d8b5fba56dabfc48ed1e04029d4
Andr3xDev <achavarropp@gmail.com>
2025-07-03 03:40:26 -0500

    chore: general clear

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
+import Lobby from "./pages/Lobby";
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-
-            <Route>{/* Futuras rutas protegidas aquí */}</Route>
-
+            <Route path="/lobby" element={<Lobby />} />{" "}
+            {/* DOTO: eliminte this debugging */}
+            <Route path="/lobby/:roomCode" element={<Lobby />} />
             <Route path="/" element={<Navigate to="/" />} />
-            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 };
