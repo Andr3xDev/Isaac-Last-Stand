e7a86420c97af990a4f7d91422d9c8aed2047ba0
Andr3xDev <achavarropp@gmail.com>
2025-07-03 03:51:48 -0500

    update: add new styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,43 @@
-import Button from "../components/Buton";
-import Menu from "../components/Menu";
+import { useNavigate } from "react-router-dom";
+import { nanoid } from "nanoid";
+import "../styles/container.css";
+import { useWebSocket } from "../hooks/useWebsocket";
 
 function Home() {
+    const navigate = useNavigate();
+    const { sendMessage } = useWebSocket();
+
+    const handleCreateLobby = () => {
+        const newRoomCode = nanoid(5).toUpperCase();
+        sendMessage({
+            type: "CREATE_ROOM",
+            roomCode: newRoomCode,
+        });
+        navigate(`/lobby/${newRoomCode}`);
+    };
+
     return (
         <div className="w-screen h-screen flex justify-center items-start">
             <div className="w-1/2 flex flex-col items-center">
                 <img
                     src="/src/assets/title.png"
                     alt="Logo game"
-                    className="w-10/12 py-6"
+                    className="w-full my-6"
                 />
-
-                <Menu>
-                    <Button label="Crear Sala" to="/lobby" />
-                    <Button label="Unirse a una sala" to="/lobby" />
-                </Menu>
+                <div className="container-menu w-11/13 flex-col flex gap-8 justify-center ">
+                    <button
+                        onClick={handleCreateLobby}
+                        className="text-4xl cursor-pointer bg-[#863832] text-white font-bold py-3 px-8 rounded-lg shadow-lg hover:bg-[#402120] transition-all duration-200"
+                    >
+                        Create Lobby
+                    </button>
+                    <button
+                        onClick={handleCreateLobby}
+                        className="text-4xl cursor-pointer bg-[#863832] text-white font-bold py-3 px-8 rounded-lg shadow-lg hover:bg-[#402120] transition-all duration-200"
+                    >
+                        Join Lobby
+                    </button>
+                </div>
             </div>
         </div>
     );
